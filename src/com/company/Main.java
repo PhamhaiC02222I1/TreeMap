9127faca4b2a5f4e54ce521a42cd0d95b251ee84
hai <humg.ola@gmail.com>
2022-04-04 10:09:12 +0700

    up

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str = "Xin chào các bạn, mình là Hương, mình quê ở Hà Nam, chào chào chào";
+        str = str.replaceAll(",", "");
+        String[] arr = str.split(" ");
+        System.out.println(str);
+        System.out.println("------------");
+        Map<String, Integer> map = new TreeMap<String, Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].toUpperCase();
+            String key = arr[i];
+            if (map.containsKey(key)) {
+                int value = map.get(key);
+                map.put(key, value + 1);
+            } else {
+                map.put(key, 1);
+            }
+            System.out.println(" từ khóa " + key + " xuất hiện " + map.get(key));
+        }
+        Set tapHop = map.keySet();// dùng set để k in ra các phần tử trùng nhau
+//        Iterator i = tapHop.iterator();// dung oterator duyệt phần tử collection
+//        while (i.hasNext()) {//hasNext trả về true nếu có nhiều phần tử kế tiếp đang duyệt
+//            String key = (String) i.next();
+//
+//        }
+
+
+    }
+}
